0fd03b88390549e57f2ee4756367a516769a1988
Jaffar Ashoor <j3ffarm@gmail.com>
2023-10-11 13:21:05 +0300

    Remove api example and table

--- stacks/DBStack.ts
+++ stacks/DBStack.ts
@@ -1,15 +1,7 @@
-import { Bucket, Table, StackContext } from "sst/constructs";
+import { Bucket, StackContext } from "sst/constructs";
 
 export function DBStack({ stack }: StackContext) {
-  // Create the table
-  const table = new Table(stack, "Counter", {
-    fields: {
-      counter: "string",
-    },
-    primaryIndex: { partitionKey: "counter" },
-  });
-
   const materialBucket = new Bucket(stack, "Material-Bucket");
 
-  return { table, materialBucket };
+  return { materialBucket };
 }
