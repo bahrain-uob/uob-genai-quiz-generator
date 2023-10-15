fd403eb2dc35a76b42b6ab6c150f65cd9f48db60
salman <salmanbri7@gmail.com>
2023-10-15 15:59:50 +0300

    Create courses DB

--- stacks/DBStack.ts
+++ stacks/DBStack.ts
@@ -1,7 +1,14 @@
-import { Bucket, StackContext } from "sst/constructs";
+import { Table, Bucket, StackContext } from "sst/constructs";
 
 export function DBStack({ stack }: StackContext) {
   const materialBucket = new Bucket(stack, "Material-Bucket");
-
-  return { materialBucket };
+  const courses_table = new Table(stack, "courses", {
+    fields: {
+      user_id: "string",
+      course_id: "string",
+      course_name: "string",
+    },
+    primaryIndex: { partitionKey: "user_id", sortKey: "course_id" },
+  });
+  return { materialBucket, courses_table };
 }
