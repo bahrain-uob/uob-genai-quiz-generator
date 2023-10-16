96fd759811408a36b094254274c7e1ff430eff86
emanaradi <aradieman@gmail.com>
2023-10-16 12:39:24 +0300

    Update step progress bar to include select course

--- packages/frontend/src/pages/QuizGeneration.tsx
+++ packages/frontend/src/pages/QuizGeneration.tsx
@@ -4,6 +4,7 @@ import Navbar from "../components/Navbar";
 import StepProgressBar from "../components/StepProgressBar";
 import MaterialsTable from "../components/MaterialsTable";
 import QuizSetupForm from "../components/QuizSetup";
+import QuestionsSetup from "../components/QuestionsSetup";
 
 function Quizzes() {
   const [stepNo, setStepNo] = useState(0);
@@ -16,10 +17,11 @@ function Quizzes() {
       </div>
       <StepProgressBar stepNo={stepNo} />
       <div className="step-container">
-        {stepNo == 0 && <MaterialsTable isSelecting={true} />}
-        {stepNo == 1 && <QuizSetupForm />}
-        {stepNo == 2 && <h1>question</h1>}
-        {stepNo == 3 && <h1>review</h1>}
+        {stepNo == 0 && <CoursesTable />}
+        {stepNo == 1 && <MaterialsTable isSelecting={true} />}
+        {stepNo == 2 && <QuizSetupForm />}
+        {stepNo == 3 && <QuestionsSetup />}
+        {stepNo == 4 && <h1>review</h1>}
       </div>
       <div style={{ display: "flex", justifyContent: "center" }}>
         {stepNo > 0 && (
@@ -37,4 +39,49 @@ function Quizzes() {
   );
 }
 
+function CoursesTable() {
+  return (
+    <>
+      <div className="courses-table">
+        <table>
+          <thead>
+            <tr className="heading">
+              <th></th>
+              <th>COURSE CODE</th>
+              <th>COURSE NAME</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td style={{ textAlign: "center" }}>
+                <input
+                  type="radio"
+                  name="id"
+                  style={{ width: "20px", height: "20px" }}
+                />
+              </td>
+              <td>ITCS448</td>
+              <td>Cloud Computing</td>
+            </tr>
+
+            <tr>
+              <td style={{ textAlign: "center" }}>
+                <input
+                  type="radio"
+                  name="id"
+                  style={{ width: "20px", height: "20px" }}
+                />
+              </td>
+              <td>ITCS 441</td>
+              <td>Parallel and Distributed Systems</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+
 export default Quizzes;
+export { CoursesTable };
