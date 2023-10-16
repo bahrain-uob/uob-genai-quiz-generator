728fee23723b81c28cf3a73b8be28933176e9751
Jaffar Ashoor <j3ffarm@gmail.com>
2023-10-16 11:57:48 +0300

    Implement GET /courses route

--- packages/api/src/courses.ts
+++ packages/api/src/courses.ts
@@ -1,9 +1,30 @@
-export async function get() {
+import { DynamoDBClient, PutItemCommand, QueryCommand } from "@aws-sdk/client-dynamodb"
+import { APIGatewayProxyEventV2 } from "aws-lambda";
+import { randomUUID } from "crypto"
+import { Table } from "sst/node/table";
+
+const dynamodb = new DynamoDBClient();
+
+export const get = async () => {
+  const command = new QueryCommand({
+    TableName: (Table as any).courses.tableName,
+    ProjectionExpression: "course_id, course_name",
+    KeyConditionExpression: "user_id = :uid",
+    ExpressionAttributeValues: {
+      // TODO get the user id from the authenticated request
+      ":uid": { "S": "1" }
+    }
+  })
+
+  let response;
+  try { response = await dynamodb.send(command) } catch { return { statusCode: 500 } };
+  let courses = response.Items?.map(obj => ({ course_id: obj.course_id.S, course_name: obj.course_name.S }));
+
   return {
     statusCode: 200,
-    body: JSON.stringify([
-      { id: 1, code: "ITCS441", name: "Parallel" },
-      { id: 2, code: "ITCS453", name: "Multimedia" },
-    ]),
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(courses)
   };
+
 }
+
