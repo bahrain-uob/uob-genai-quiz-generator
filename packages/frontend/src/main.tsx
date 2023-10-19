a2192507150ab5c26110e28d51b3973638a704fe
emanaradi <aradieman@gmail.com>
2023-10-19 23:36:28 +0300

    Add quizzes page

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -4,8 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage.tsx";
 import Materials from "./pages/Materials.tsx";
 import Courses from "./pages/Courses.tsx";
-import Quizzes from "./pages/QuizGeneration.tsx";
-
+import QuizGeneration from "./pages/QuizGeneration.tsx";
+import Quizzes from "./pages/Quizzes";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
@@ -13,7 +13,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<Homepage />} />
         <Route path="/Materials" element={<Materials />} />
         <Route path="/Courses" element={<Courses />} />
-        <Route path="/createQuiz" element={<Quizzes />} />
+        <Route path="/createQuiz" element={<QuizGeneration />} />
+        <Route path="/quizzes" element={<Quizzes />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
