5f15a8d8e5d89704a6eb66bc9190fbb456c9cf15
emanaradi <aradieman@gmail.com>
2023-10-24 16:20:50 +0300

    Add modal for uploading in materials page

--- packages/frontend/src/pages/Materials.tsx
+++ packages/frontend/src/pages/Materials.tsx
@@ -1,19 +1,71 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import MaterialsTable from "../components/MaterialsTable";
 import Navbar from "../components/Navbar";
 import Titles from "../components/Title";
 import Upload from "../components/Upload";
-
+import Modal from "react-modal";
+import { useState } from "react";
+import { faX, faCloudArrowUp } from "@fortawesome/free-solid-svg-icons";
 function Materials() {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div>
       <Navbar />
       <div className="top-materials">
         <Titles title={["ITCS448 Cloud Computing", "Course Content"]} />
-        <Upload />
+        <button className="upload-button-1" onClick={openModal}>
+          <FontAwesomeIcon
+            icon={faCloudArrowUp}
+            onClick={closeModal}
+            className="x-icon"
+            size="xl"
+            style={{ color: "white" }}
+          />
+        </button>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          contentLabel="Upload Material Modal"
+          style={bg}
+        >
+          <div className="x">
+            <FontAwesomeIcon
+              icon={faX}
+              onClick={closeModal}
+              className="x-icon"
+              size="xl"
+              style={{ color: "#C7C7C7" }}
+            />
+          </div>
+          <div className="modal-content">
+            <h3>Upload Course Content</h3>
+            <Upload />
+            <button className="upload-button-2">Upload</button>
+          </div>
+        </Modal>
       </div>
       <MaterialsTable isSelecting={false} />
     </div>
   );
 }
-
+const bg = {
+  content: {
+    background: "#F5F5F5",
+  },
+  // overlay: {
+  //   top: 40,
+  //   left: 280,
+  //   right: 280,
+  //   bottom: 40,
+  //   background: "rgba(245, 39, 145, 0)",
+  // },
+};
 export default Materials;
