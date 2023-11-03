11513b93b17680ab01b04a277d3f68c52e6de5ea
emanaradi <aradieman@gmail.com>
2023-11-03 22:38:14 +0300

    Implement api in courses and materials

--- packages/frontend/src/pages/QuizGeneration.tsx
+++ packages/frontend/src/pages/QuizGeneration.tsx
@@ -1,32 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Titles from "../components/Title";
 import Navbar from "../components/Navbar";
 import StepProgressBar from "../components/StepProgressBar";
 import MaterialsTable from "../components/MaterialsTable";
 import QuizSetupForm from "../components/QuizSetup";
 import QuestionsSetup from "../components/QuestionsSetup";
+import { API } from "aws-amplify";
 
 function Quizzes() {
   const [stepNo, setStepNo] = useState(0);
 
+  const courseId = localStorage.getItem("courseId")!;
   return (
     <>
       <Navbar />
       <div className="context">
-        <Titles title={["ITCS448 CLOUD COMPUTING", "Create quiz"]} />
+        <Titles title={["Create Quiz"]} />
       </div>
       <StepProgressBar stepNo={stepNo} />
       <div className="step-container">
         {stepNo == 0 && <CoursesTable />}
-        {stepNo == 1 && <MaterialsTable isSelecting={true} />}
+        {stepNo == 1 && (
+          <MaterialsTable courseId={courseId} isSelecting={true} />
+        )}
         {stepNo == 2 && <QuizSetupForm />}
         {stepNo == 3 && <QuestionsSetup />}
         {stepNo == 4 && <h1>review</h1>}
       </div>
       <div style={{ display: "flex", justifyContent: "center" }}>
         {stepNo > 0 && (
-          <button className="previous" onClick={() => setStepNo(stepNo - 1)}>
-            Previous
+          <button
+            className="previous"
+            onClick={() => {
+              setStepNo(stepNo - 1);
+            }}
+          >
+            Back
           </button>
         )}
         {stepNo < 4 && (
@@ -39,7 +48,25 @@ function Quizzes() {
   );
 }
 
+interface Course {
+  id: string;
+  code: string;
+  name: string;
+}
 function CoursesTable() {
+  const [courses, setCourses] = useState([] as Course[]);
+  useEffect(() => {
+    updateCourses();
+  }, []);
+
+  const updateCourses = async () => {
+    let courses = await API.get("api", "/courses", {});
+    setCourses(courses);
+  };
+
+  function selectCourse(courseId: string) {
+    localStorage.setItem("courseId", courseId);
+  }
   return (
     <>
       <div className="courses-table">
@@ -53,29 +80,20 @@ function CoursesTable() {
           </thead>
 
           <tbody>
-            <tr>
-              <td style={{ textAlign: "center" }}>
-                <input
-                  type="radio"
-                  name="id"
-                  style={{ width: "20px", height: "20px" }}
-                />
-              </td>
-              <td>ITCS448</td>
-              <td>Cloud Computing</td>
-            </tr>
-
-            <tr>
-              <td style={{ textAlign: "center" }}>
-                <input
-                  type="radio"
-                  name="id"
-                  style={{ width: "20px", height: "20px" }}
-                />
-              </td>
-              <td>ITCS 441</td>
-              <td>Parallel and Distributed Systems</td>
-            </tr>
+            {courses.map((course) => (
+              <tr>
+                <td style={{ textAlign: "center" }}>
+                  <input
+                    type="radio"
+                    name="course"
+                    style={{ width: "20px", height: "20px" }}
+                    onClick={() => selectCourse(course.id)}
+                  />
+                </td>
+                <td>{course.code}</td>
+                <td>{course.name}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
