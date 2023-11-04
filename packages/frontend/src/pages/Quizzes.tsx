fd8ea0e5db7fd147e8c6eeca00ad7ccbc1efe558
emanaradi <aradieman@gmail.com>
2023-11-04 21:36:06 +0300

    Integerate with backend

--- packages/frontend/src/pages/Quizzes.tsx
+++ packages/frontend/src/pages/Quizzes.tsx
@@ -2,9 +2,38 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Navrbar from "../components/Navbar";
 import Title from "../components/Title";
 import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { API } from "aws-amplify";
+import { useEffect, useState } from "react";
+
+interface Course {
+  id: string;
+  code: string;
+  name: string;
+}
 
 function Quizzes() {
+  const [courses, setCourses] = useState([] as Course[]);
+  useEffect(() => {
+    updateCourses();
+  }, []);
+
+  const updateCourses = async () => {
+    let courses = await API.get("api", "/courses", {});
+    setCourses(courses);
+  };
+
+  const nav = useNavigate();
+  function navigate(
+    course_id: string,
+    course_code: string,
+    course_name: string
+  ) {
+    localStorage.setItem("courseId", course_id);
+    localStorage.setItem("courseCode", course_code);
+    localStorage.setItem("courseName", course_name);
+    nav("/materials");
+  }
   return (
     <>
       <Navrbar />
@@ -16,22 +45,24 @@ function Quizzes() {
       </div>
 
       <div className="container">
-        <div className="course-quiz-container">
-          <h2>ITCS448 Cloud Computing</h2>
-          <div className="quizzes-container">
-            <Quiz name="Quiz 1" date="18th Oct 2023" />
-            <Quiz name="Quiz 2" date="18th Oct 2023" />
-            <Quiz name="Quiz 3" date="18th Oct 2023" />
+        {courses.map((course) => (
+          <div className="course-quiz-container">
+            <h2
+              className="underlined"
+              onClick={() => {
+                navigate(course.id, course.code, course.name);
+              }}
+            >
+              {`${course.code}  - ${course.name}`}
+            </h2>
+
+            <div className="quizzes-container">
+              <Quiz name="Quiz 1" date="18th Oct 2023" />
+              <Quiz name="Quiz 2" date="18th Oct 2023" />
+              <Quiz name="Quiz 3" date="18th Oct 2023" />
+            </div>
           </div>
-        </div>
-        <div className="course-quiz-container">
-          <h2>ITCS444 Mobile Application</h2>
-          <div className="quizzes-container">
-            <Link to="/createquiz">
-              <button className="generate-button-2">Generate Quiz</button>
-            </Link>
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
