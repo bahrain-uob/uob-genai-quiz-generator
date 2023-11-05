79350619078cc83c542fdbb3c48b421b05d8387f
salman <salmanbri7@gmail.com>
2023-11-05 15:54:13 +0300

    Add full prompt example

--- packages/api/src/questionsGen.py
+++ packages/api/src/questionsGen.py
@@ -38,7 +38,18 @@ Ronaldo began his professional career with Sporting CP at age 17 in 2002, and si
 # Perception
 # Linguistic Intelligence
 # """
-# response = s3.get_object(Bucket=TEXT_BUCKET, Key=object_key)
+# topic = """
+# Amazon S3 or Amazon Simple Storage Service is a service offered by Amazon Web Services (AWS) that provides object storage through a web service interface.Amazon S3 uses the same scalable storage infrastructure that Amazon.com uses to run its e-commerce network. Amazon S3 can store any type of object, which allows uses like storage for Internet applications, backups, disaster recovery, data archives, data lakes for analytics, and hybrid cloud storage. AWS launched Amazon S3 in the United States on March 14, 2006, then in Europe in November 2007.
+# """
+topic = """
+
+Amazon Web Services, Inc. (AWS) is a subsidiary of Amazon that provides on-demand cloud computing platforms and APIs to individuals, companies, and governments, on a metered, pay-as-you-go basis.
+Clients will often use this in combination with autoscaling (a process that allows a client to use more computing in times of high application usage, and then scale down to reduce costs when there is less traffic). 
+These cloud computing web services provide various services related to networking, compute, storage, middleware, IoT and other processing capacity, as well as software tools via AWS server farms. 
+This frees clients from managing, scaling, and patching hardware and operating systems. 
+One of the foundational services is Amazon Elastic Compute Cloud (EC2), which allows users to have at their disposal a virtual cluster of computers, with extremely high availability, which can be interacted with over the internet via REST APIs, a CLI or the AWS console.
+
+"""
 import re
 
 number = 2
@@ -58,7 +69,7 @@ def create(prompt):
             {
                 "inputs": prompt,
                 "parameters": {
-                    "max_new_tokens": 128,
+                    "max_new_tokens": 265,
                     "top_p": 0.9,
                     "temperature": 0.7,
                     "return_full_text": False,
@@ -69,56 +80,125 @@ def create(prompt):
     )
     result = json.loads(response["Body"].read().decode())[0]["generation"]
     print(result)
-    parse = re.search(r"{\n(.*?)}", result, re.DOTALL).group(0)
-    print("--------------------------------------------")
-    print(parse)
-    return parse
+    # parse = re.search(r"{\n(.*?)}", result, re.DOTALL).group(0)
+    # print("--------------------------------------------")
+    # print(parse)
+    # return parse
 
 
 def MCQ(event, context):
     number = event["number"]
     # topic = event["key"]
     prompt = """
-    Human: i want you to generate 1 MCQ question about Albert Einestein
+    Human: i want you to generate 3 MCQ question about  this :
+    Cristiano Ronaldo dos Santos Aveiro:  born 5 February 1985), better known as Ronaldo, is a Portuguese professional footballer who plays as a forward. He is the captain of the Portuguese national team and he is currently playing at Saudi Arabian football club Al Nassr.
+    He is considered to be one of the greatest footballers of all time, and, by some, as the greatest ever.
+    Ronaldo began his professional career with Sporting CP at age 17 in 2002, and signed for Manchester United a year later. He won three back-to-back Premier League titles: in 2006-07, 2007-08, and 2008-09. In 2007-08, Ronaldo, helped United win the UEFA Champions League. In 2008-09, he won his first FIFA Club World Cup in December 2008, and he also won his first Ballon d'Or. At one point Ronaldo was the most expensive professional footballer of all time, after moving from Manchester United to Real Madrid for approximately £80 m in July 2009
+
     The output should be a code snippet formatted in the following schema with only one correct answer:
     {
       "question": string // the question
       "choices": string // an array containg all the choices of the question, with only one correct choice, the others should be wrong
       "correct_answer_index": string // the index of the correct choice assuming 0-indexing
-    }
-    Assistant:
+    },
     {
-      "question": "What is Albert Einstein's most famous equation?",
-      "choices": ["F=ma", "G=Tc^3", "E=mc^2", "a=b/c"],
+      "question": string // the question
+      "choices": string // an array containg all the choices of the question, with only one correct choice, the others should be wrong
+      "correct_answer_index": string // the index of the correct choice assuming 0-indexing
+    },
+    {
+      "question": string // the question
+      "choices": string // an array containg all the choices of the question, with only one correct choice, the others should be wrong
+      "correct_answer_index": string // the index of the correct choice assuming 0-indexing
+    },
+    ]
+    Assistant:
+    [
+    {
+      "question": "what is Ronaldo's birthplace?",
+      "choices": ["germany", "Portugal", "Spain", "England"],
+      "correct_answer_index": 1
+    },
+    {
+      "question": "Where does Ronaldo currently plays?",
+      "choices": ["Liverpool", "Barcelona", "Saudi Arabian football club Al Nassr", "Real madrid"],
       "correct_answer_index": 2
+    },
+    {
+      "question": "When was Ronaldo born?",
+      "choices": ["5 February 1985", "5 February 1986", "4 February 1985", "6 February 1985"],
+      "correct_answer_index": 0
     }
-    Human: i want you to generate 1 MCQ question about python
-    The output should be a code snippet formatted in the following schema with only one correct answer
+    ]
+    Human: i want you to generate 3 MCQ question about  this :
+    Apple Inc. is an American multinational technology company headquartered in Cupertino, California.
+    As of March 2023, Apple is the world's biggest company by market capitalization, and with US$394.3 billion the largest technology company by 2022 revenue. 
+    As of June 2022, Apple is the fourth-largest personal computer vendor by unit sales; the largest manufacturing company by revenue; and the second-largest mobile phone manufacturer in the world. 
+    It is considered one of the Big Five American information technology companies, alongside Alphabet (parent company of Google), Amazon, Meta, and Microsoft.
+    Apple was founded as Apple Computer Company on April 1, 1976, by Steve Wozniak, Steve Jobs and Ronald Wayne to develop and sell Wozniak's Apple I personal computer. 
+    It was incorporated by Jobs and Wozniak as Apple Computer, Inc. in 1977. 
+
+    The output should be a code snippet formatted in the following schema with only one correct answer:
+    [{
+      "question": string // the question
+      "choices": string // an array containg all the choices of the question, with only one correct choice, the others should be wrong
+      "correct_answer_index": string // the index of the correct choice assuming 0-indexing
+    },
     {
       "question": string // the question
       "choices": string // an array containg all the choices of the question, with only one correct choice, the others should be wrong
       "correct_answer_index": string // the index of the correct choice assuming 0-indexing
-    }
+    },
+    {
+      "question": string // the question
+      "choices": string // an array containg all the choices of the question, with only one correct choice, the others should be wrong
+      "correct_answer_index": string // the index of the correct choice assuming 0-indexing
+    },
+    ]
+
     Assistant:
+    [
     {
-      "question": "What is the correct syntax to print a string in Python?",
-      "choices": ["print('string')", "print \"string\"", "printf('string')", "console.log('string')"],
-      "correct_answer_index": 0  
+      "question": "When was Apple the world's biggest company by market capitilization?",
+      "choices": ["March 2023", "June 2023", "May 2023", "Marhc 2022"],
+      "correct_answer_index": 0
+    },
+    {
+      "question": "how much is apple revenue?",
+      "choices": ["390 billion", "400 billion", "349.3 billion", "394.3 billion"],
+      "correct_answer_index": 3
+    },
+    {
+      "question": "Apple was not founded by",
+      "choices": ["john doe", "Steve Jobs", "Ronald Wayne", "Steve Wozniak"],
+      "correct_answer_index": 0
     }
-    
-    Human: i want you to generate 1 MCQ question about this : {{topic}}
+    ]
+    Human: i want you to generate 3 MCQ question about this : {{topic}}
     The output should be a code snippet formatted in the following schema with only one correct answer
+    [{
+      "question": string // the question
+      "choices": string // an array containg all the choices of the question, with only one correct choice, the others should be wrong
+      "correct_answer_index": string // the index of the correct choice assuming 0-indexing
+    },
     {
       "question": string // the question
       "choices": string // an array containg all the choices of the question, with only one correct choice, the others should be wrong
       "correct_answer_index": string // the index of the correct choice assuming 0-indexing
-    }
+    },
+    {
+      "question": string // the question
+      "choices": string // an array containg all the choices of the question, with only one correct choice, the others should be wrong
+      "correct_answer_index": string // the index of the correct choice assuming 0-indexing
+    },
+    ]
+
     Assistant:"""
     questions = []
     prompt = prompt.replace("{{topic}}", topic)
     for index in range(int(number)):
         ali = create(prompt)
-        questions.append(json.loads(ali))
+        # questions.append(json.loads(ali))
         # prompt= prompt + json.dumps(ali ,indent=2) + "\nAssistant:" #test
 
     return questions
@@ -135,7 +215,7 @@ def TF(event, context):
     }
     Assistant:
     {
-      "question": " is Albert Einstein's most famous equation E=mc^2 ?",
+      "question": "True or false: Albert Einstein's most famous equation is E=mc^2 ?",
       "correct_answer": "True"
     }
     Human: i want you to generate 1 true or false question about python
@@ -146,10 +226,10 @@ def TF(event, context):
     }
     Assistant:
     {
-      "question": " is the correct syntax to print a string in Python console.log('string')",
+      "question": "True or false: the correct syntax to print a string in Python is console.log('string')",
       "correct_answer": "False"   
     }
-    Human: i want you to generate 1 true or false question about {{topic}}
+    Human: i want you to generate 1 true or false question about: {{topic}}
     The output should be a code snippet formatted in the following schema with only one correct answer
     {
       "question": string // the true or false question
