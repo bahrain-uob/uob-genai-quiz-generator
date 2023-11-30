7932fd93921854d513ffcdb9ed1e6e38e6b2614c
Jaffar Ashoor <j3ffarm@gmail.com>
2023-11-30 15:16:26 +0300

    Fix stacks import error

--- stacks/FrontendStack.ts
+++ stacks/FrontendStack.ts
@@ -4,7 +4,7 @@ import { ApiStack } from "./ApiStack";
 import { AuthStack } from "./AuthStack";
 
 export function FrontendStack({ stack, app }: StackContext) {
-  const { api } = use(ApiStack);
+  const { api, socket } = use(ApiStack);
   const { auth } = use(AuthStack);
   const { materialBucket } = use(CoreStack);
 
