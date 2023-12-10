67bb58586ba3284adc7357372d3da38b9fd64d5e
Jaffar Ashoor <j3ffarm@gmail.com>
2023-12-10 20:48:57 +0300

    Fix accepted file types

--- packages/frontend/src/pages/Materials.tsx
+++ packages/frontend/src/pages/Materials.tsx
@@ -112,7 +112,16 @@ function Materials() {
                 maxFileCount={10}
                 accessLevel="public"
                 path={`${userId}/${course_id}/materials/`}
-                acceptedFileTypes={["pdf", "docx", "pptx", "mp4", "txt"]}
+                acceptedFileTypes={[
+                  "pdf",
+                  "docx",
+                  "pptx",
+                  "mp4",
+                  "txt",
+                  "png",
+                  "jpeg",
+                  "jpg",
+                ]}
               />
             </div>
           </div>
