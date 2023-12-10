e581e0385339ba8b565c05194c79dd2a9b35adc4
Jaffar Ashoor <j3ffarm@gmail.com>
2023-12-10 16:25:02 +0300

    Fix proccess image region

--- packages/functions/src/process_image.py
+++ packages/functions/src/process_image.py
@@ -1,34 +1,28 @@
 import boto3
 import os
-import time
 import urllib
 
 OUTPUT_BUCKET = os.environ["OUTPUT_BUCKET"]
 
 
 def lambda_handler(event, context):
-    textract = boto3.client("textract",region_name="us-east-1")
-    s3 = boto3.client("s3",region_name="me-south-1")
+    textract = boto3.client("textract", region_name="us-east-1")
+    s3 = boto3.client("s3")
 
     bucket = event["Records"][0]["s3"]["bucket"]["name"]
     key = urllib.parse.unquote_plus(event["Records"][0]["s3"]["object"]["key"])
-    document_location = {"S3Object": {"Bucket": bucket, "Name": key}}
 
     response = s3.get_object(Bucket=bucket, Key=key)
-    object_content = response['Body'].read()
+    object_content = response["Body"].read()
 
     try:
-        response = textract.detect_document_text(
-            Document={
-                'Bytes': object_content
-            }
-        )
+        response = textract.detect_document_text(Document={"Bytes": object_content})
 
-        blocks = response['Blocks']
+        blocks = response["Blocks"]
         extracted_text = ""
         for block in blocks:
-            if block['BlockType'] == 'LINE':
-                extracted_text += block['Text'] + "\n"
+            if block["BlockType"] == "LINE":
+                extracted_text += block["Text"] + "\n"
 
         output_key = f"{key}.txt"
 
@@ -36,8 +30,9 @@ def lambda_handler(event, context):
             Body=extracted_text,
             Bucket=OUTPUT_BUCKET,
             Key=output_key,
-            ContentType="text/plain"
+            ContentType="text/plain",
         )
 
     except Exception as e:
         print(f"Error: {str(e)}")
+
